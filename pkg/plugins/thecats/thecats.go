0c3bb064610e90377855338f5311f114457f2be5
dezhiShen <1179551960@qq.com>
2021-03-17 12:36:28 +0800

    feat(thecat):增加猫猫图

---
+++ pkg/plugins/thecats/thecats.go
@@ -0,0 +1,102 @@
+package thecats
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/Logiase/MiraiGo-Template/bot"
+	"github.com/Mrs4s/MiraiGo/message"
+	"github.com/dezhiShen/MiraiGo-Bot/pkg/plugins"
+)
+
+var url = "https://api.thecatapi.com/v1/images/search?limit=1&size=med"
+
+// Plugin 猫猫图
+type Plugin struct {
+	plugins.NoSortPlugin
+	plugins.NoInitPlugin
+	plugins.AlwaysNotFireNextEventPlugin
+}
+
+// PluginInfo PluginInfo
+func (w Plugin) PluginInfo() *plugins.PluginInfo {
+	return &plugins.PluginInfo{
+		ID:   "thecat",
+		Name: "猫猫图",
+	}
+}
+
+// IsFireEvent 是否触发
+func (w Plugin) IsFireEvent(msg *plugins.MessageRequest) bool {
+	if len(msg.Elements) == 1 && msg.Elements[0].Type() == message.Text {
+		v := msg.Elements[0]
+		field, ok := v.(*message.TextElement)
+		return ok && field.Content == ".thecat"
+	}
+	return false
+}
+
+// OnMessageEvent OnMessageEvent
+func (w Plugin) OnMessageEvent(request *plugins.MessageRequest) (*plugins.MessageResponse, error) {
+	result := &plugins.MessageResponse{
+		Elements: make([]message.IMessageElement, 1),
+	}
+	b, e := getCatPic()
+	if e != nil {
+		return nil, e
+	}
+	gimage, err := bot.Instance.UploadGroupImage(request.GroupCode, bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+	result.Elements[0] = gimage
+	return result, nil
+}
+
+func init() {
+	plugins.RegisterOnMessagePlugin(Plugin{})
+}
+
+type Data struct {
+	Breeds []string `json:"breeds"`
+	ID     string   `json:"id"`
+	Url    string   `json:"url"`
+}
+
+func getCatPic() ([]byte, error) {
+	r, err := http.DefaultClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	robots, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	r.Body.Close()
+	resp := string(robots)
+	if resp == "" {
+		return nil, errors.New("请稍后重试")
+	}
+
+	var datas []Data
+	err = json.Unmarshal(robots, &datas)
+	if err != nil {
+		return nil, err
+	}
+	if datas == nil || len(datas) == 0 {
+		return nil, errors.New("请稍后重试")
+	}
+	imageResp, err := http.DefaultClient.Get(datas[0].Url)
+	if err != nil {
+		return nil, err
+	}
+	imageBytes, err := ioutil.ReadAll(imageResp.Body)
+	if err != nil {
+		return nil, err
+	}
+	imageResp.Body.Close()
+	return imageBytes, nil
+}
