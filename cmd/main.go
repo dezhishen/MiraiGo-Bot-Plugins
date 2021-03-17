cd3aa8495ebfc88929d008da73fb32428c6c8431
dezhiShen <1179551960@qq.com>
2021-03-17 09:37:48 +0800

    mod(jrrp):启用插件

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/dezhiShen/MiraiGo-Bot-Plugins/pkg/plugins/calendar"
 	_ "github.com/dezhiShen/MiraiGo-Bot-Plugins/pkg/plugins/hitokoto"
+	_ "github.com/dezhiShen/MiraiGo-Bot-Plugins/pkg/plugins/jrrp"
 	_ "github.com/dezhiShen/MiraiGo-Bot-Plugins/pkg/plugins/random"
 	_ "github.com/dezhiShen/MiraiGo-Bot-Plugins/pkg/plugins/tips"
 	_ "github.com/dezhiShen/MiraiGo-Bot-Plugins/pkg/plugins/weather"
