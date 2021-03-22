61540e0a3662ab74817d0438c4825aacb87d1bee
dezhiShen <1179551960@qq.com>
2021-03-22 09:46:10 +0800

    feat(彩虹屁):添加key

--- scripts/deploy_docker_test.sh
+++ scripts/deploy_docker_test.sh
@@ -2,4 +2,4 @@ git pull
 docker build . -t 1179551960sdniu/miraigo:test
 docker stop miraigo
 docker rm miraigo
-docker run -d --restart=always --name=miraigo -e TZ=Asia/Shanghai -v /docker_data/miraigo/:/data 1179551960sdniu/miraigo:test
+docker run -d -e BOT_DJT_KEY=9bc86e70f1c8f0d9 -e BOT_CHP_KEY=ce3552aa350641f0 --restart=always --name=miraigo -e TZ=Asia/Shanghai -v /docker_data/miraigo/:/data 1179551960sdniu/miraigo:test
